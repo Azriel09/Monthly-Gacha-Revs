15bfff4cf2589226fcc4647939fd5c7d7e945e2e
Azuriyale <Azuriyale@users.noreply.github.com>
2023-08-15 14:27:34 +0800

    added themed primereact table. still deciding which table to use

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.scss";
+// import "primereact/resources/themes/viva-dark/theme.css";
+// import "primereact/resources/themes/soho-dark/theme.css";
+
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
