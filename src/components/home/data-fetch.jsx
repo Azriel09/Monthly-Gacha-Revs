b5ad3b6fd611e897512488775a546fce40c3a264
Azuriyale <Azuriyale@users.noreply.github.com>
2023-08-23 14:05:13 +0800

    Used react query to cache fetched data

--- src/components/home/data-fetch.jsx
+++ src/components/home/data-fetch.jsx
@@ -1,21 +1,52 @@
 import React, { useEffect, useState } from "react";
 import GachaTable from "./table";
-export default function GetData() {
-  const [gachaGames, setGachaGames] = useState();
+import Loading from "../loading";
+import { useQuery } from "@tanstack/react-query";
 
-  useEffect(() => {
-    fetch("http://localhost:8000/games")
-      .then((res) => res.json(res))
-      .then((data) => {
-        setGachaGames(data);
-      })
-      .catch((err) => {
-        console.log(err.message);
-      });
-  }, []);
+export default function GetData() {
+  // const [gachaGames, setGachaGames] = useState();
+  // const [isLoading, setIsLoading] = useState(true);
+  // const months = ["July 2023", "June 2023", "May 2023"];
+
+  // useEffect(() => {
+  //   console.log("isFetching");
+  //   fetch("http://localhost:8000/games")
+  //     .then((res) => res.json(res))
+  //     .then((data) => {
+  //       setGachaGames(data);
+  //       setIsLoading(false);
+  //     })
+  //     .catch((err) => {
+  //       console.log(err.message);
+  //     });
+  // }, []);
+
+  function fetchData() {
+    return useQuery({
+      queryKey: ["games"],
+      queryFn: () => {
+        const data = fetch("http://localhost:8000/games").then((res) =>
+          res.json(res).then((resdata) => {
+            return resdata;
+          })
+        );
+
+        return data;
+      },
+    });
+  }
+
+  const { status, data, error, isFetching } = fetchData();
+
   return (
     <React.Fragment>
-      {gachaGames && <GachaTable fetchedData={gachaGames} />}
+      {status === "loading" ? (
+        <Loading />
+      ) : status === "error" ? (
+        <h2>Error</h2>
+      ) : (
+        <GachaTable fetchedData={data} />
+      )}
     </React.Fragment>
   );
 }
---
+++ src/components/home/select-month.jsx
@@ -0,0 +1,8 @@
+import { useState } from "react";
+
+export default function SelectMonth() {
+  const [selectedMonth, setSelectedMonth] = useState(0);
+
+  return;
+  <></>;
+}
