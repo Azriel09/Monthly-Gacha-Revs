4bdb5db3e5cf388cf1aa7b726692507b6dea9867
Azuriyale <Azuriyale@users.noreply.github.com>
2023-08-25 22:58:01 +0800

    adding lightmode/darkmode feature

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,20 +1,28 @@
 import { Link, Outlet } from "react-router-dom";
 import { Box } from "@mui/material";
 import "./navbar.scss";
+import SwitchTheme from "./switch/switch-theme";
 export default function NavBar() {
   return (
     <>
       <div className="navigation">
         <Box className="nav-links-container">
-          <Link className="nav-link" to="/">
-            HOME
-          </Link>
-          <Link className="nav-link" to="/charts">
-            CHARTS
-          </Link>
-          <Link className="nav-link" to="/about">
-            ABOUT
-          </Link>
+          <Box></Box>
+          <Box>
+            <Link className="nav-link" to="/">
+              HOME
+            </Link>
+            <Link className="nav-link" to="/charts">
+              CHARTS
+            </Link>
+            <Link className="nav-link" to="/about">
+              ABOUT
+            </Link>
+          </Box>
+          <Box sx={{ width: "160px", height: "50px" }}>
+            {" "}
+            <SwitchTheme />
+          </Box>
         </Box>
       </div>
       <Outlet />
