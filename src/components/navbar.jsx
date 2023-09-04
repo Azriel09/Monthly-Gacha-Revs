b8aaf2a1616dac14ab645623decb606276680fec
Azuriyale <Azuriyale@users.noreply.github.com>
2023-09-04 19:44:26 +0800

    navbar links now changes in light/darkmode

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,21 +1,35 @@
 import { Link, Outlet } from "react-router-dom";
 import { Box } from "@mui/material";
 import "./navbar.scss";
-import SwitchTheme from "./switch/switch-theme";
-export default function NavBar() {
+
+import { useState } from "react";
+export default function NavBar({ mode }) {
+  const [darkmode, setDarkmode] = useState(mode);
   return (
     <>
       <div className="navigation">
         <Box className="nav-links-container">
           <Box sx={{ width: 160, height: 50 }}></Box>
           <Box>
-            <Link className="nav-link" to="/">
+            <Link
+              className="nav-link"
+              to="/"
+              style={mode ? { color: "white" } : { color: "black" }}
+            >
               HOME
             </Link>
-            <Link className="nav-link" to="/charts">
+            <Link
+              className="nav-link"
+              to="/charts"
+              style={mode ? { color: "white" } : { color: "black" }}
+            >
               CHARTS
             </Link>
-            <Link className="nav-link" to="/about">
+            <Link
+              className="nav-link"
+              to="/about"
+              style={mode ? { color: "white" } : { color: "black" }}
+            >
               ABOUT
             </Link>
           </Box>
